2a4475900e45ac3d43c76e8f795c270dc3c12640
ericschmidutz <e-schmidutz@onu.edu>
2024-04-02 01:37:32 -0400

    Displays the 10 random 4 digit values.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,32 @@
 
 using namespace std;
 
+int ReturnRandomNum(int size_of_number);
+
 int main() {
+	srand(time(0));
+	const int SIZE_OF_ARRAY = 10;
+	const int SIZE_OF_DIGITS = 4;
+
+
+	int array[SIZE_OF_ARRAY] = { 1,2,3,4,5,6,7,8,9,10 };
+
+	for (int i = 0; i < SIZE_OF_ARRAY; i++) {
+		array[i] = ReturnRandomNum(SIZE_OF_ARRAY);
+	}
+
+	cout << "The random numbers:";
+
+	for (int i = 0; i < SIZE_OF_ARRAY; i++) {
+		cout << " " << array[i];
+	}
+
+	
 
 	return 0;
+}
+
+int ReturnRandomNum(int size_of_number) {
+	int random = (rand() % 9 + 1) * 1000 + (rand() % 10) * 100 + (rand() % 10) * 10 + rand() % 10;
+	return random;
 }
